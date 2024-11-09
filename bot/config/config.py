2b11a6d8a282800bda3696904bf12b066bf367ab
Orangepranik <94443455+Orangepranik@users.noreply.github.com>
2024-11-09 18:03:49 +0200

    Update config.py

--- bot/config/config.py
+++ bot/config/config.py
@@ -1,8 +1,8 @@
 
 class Settings:
 
-    API_ID: int = 28274855
-    API_HASH: str = '5ed84faa6bd3d00e2519b9a8af8901f1'
+    API_ID: int = vvodi svoi
+    API_HASH: str = 'vvodi svoi'
 
     auto_farm: bool = True
 
